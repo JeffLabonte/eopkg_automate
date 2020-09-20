af396085f383ba93d24f9c3b606de4e7cc15cc5c
Jeff Labonte <grimsleepless@protonmail.com>
2020-09-20 13:46:15 -0400

    Extract action from filepath

--- eopkg_automate.py
+++ eopkg_automate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from pathlib import Path
 from glob import glob
 from subprocess import Popen
 from typing import List
@@ -18,8 +19,16 @@ COMMANDS_MAPPING = {
 }
 
 
+def extract_action_from_path(path: Path) -> str:
+    filename =  str(path).split('/')[-1]
+    after_underscore_index = filename.find('_') + 1
+    dot_index = filename.find('.')
+    return filename[after_underscore_index:dot_index]
+
+
 def get_package_lists_path() -> List:
     return [p for p in glob('package_lists/package_*.txt', recursive=True) if '.template' not in p]
+
 
 
 def main(package_lists_path: List):
